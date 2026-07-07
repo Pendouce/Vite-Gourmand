f1e8488dcb6cac882fc81cd47c8bc652613cdded
Penda <pendapro@live.fr>
2026-07-07 18:37:33 +0200

    creation du router, classe Controlleur pour afficher la page apartenant a la route

---
+++ config/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+  Contient l'ensemble des route disponible de notre apli
+    Essayer de retourner un tableau de routes
+    'chemin' => ["controller" => "namespace/classe" => "methode", "page"]
+*/
+
+return[
+
+];
---
+++ src/Controller/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+/* 
+  Controlleur de page "static" a propos, mentions legal ...
+*/
+
+class PageController extends Controller{
+
+
+}
