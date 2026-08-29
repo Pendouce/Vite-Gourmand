4063373fe6b40b4914795c700e5c2aff57904907
Penda <pendapro@live.fr>
2026-08-29 02:31:57 +0200

    nettoyage index.php

--- public/index.php
+++ public/index.php
@@ -5,9 +5,6 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Dotenv\Dotenv;
 use App\Routing\Router;
 
-
-/* use DateTimeImmutable;
-//use DateTimeZone; */
 define('APP_ROOT', dirname(__DIR__));
 
 define('ADRESSE_VG', '27 Rue des Fauvettes, 33000 Bordeaux');
@@ -33,46 +30,3 @@ $dotenv->load();
 
 $router = new Router();
 $router->GererRequette($_SERVER['REQUEST_URI']);
-
-
-//echo rand(100, 9999);
-//$date = new DateTimeImmutable()->format('Y-m-d H:i:s');
-/* $date = new DateTimeImmutable('now', new DateTimeZone('Europe/Paris'));
-$dateRetour = $date->modify('+10 days');
-echo $dateRetour->format('Y-m-d H:i:s');
-echo $date->format('Y-m-d H:i:s'); */
-
-
-
-/* use App\Service\CalculPrixService;
-$totalBoisson = [];
-//$boissons = [$boisson1[], $boisson2[]];
-$boissons = [
-
-$boisson1 = [
-  'prix_boisson' => 10,
-  'quantite' => 5,
-],
-$boisson2 = [
-  'prix_boisson' => 10,
-  'quantite' => 10,
-]
-];
-$calcul = new CalculPrixService();
-
-foreach($boissons as $boisson){
-  $totalBoisson[] = $calcul->calculerTotalBoisson($boissons);
-}
-
- var_dump($totalBoisson); */
-/*
-$presta = [10, 5, 5,10];
-$calcul->calculTotalpresta($presta); */
-
-/* $depart = $calcul->geocoderAdresse("10 place de la bourse bordeaux");
-$arrive = $calcul->geocoderAdresse(" place gambetta bordeaux");
-
-/* var_dump($depart);
-var_dump($arrive); */
-
-//echo $calcul->calculerDistance($depart, $arrive);
