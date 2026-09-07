38bcabe049d8232c9e57200fe3d2c0ccc2742f1b
Penda <pendapro@live.fr>
2026-09-07 05:24:10 +0200

    Conversion mobile first footer, banniere

--- templates/layouts/pageBanner.php
+++ templates/layouts/pageBanner.php
@@ -1,11 +1,11 @@
 <?php /** @var string $titre */ ?>
-<div class="bg-fond-banniere py-10">
+<div class="bg-fond-banniere py-7 lg:py-10">
   <div class="flex w-full justify-center items-center relative">
     <div>
-      <img class="h-30 w-50 object-cover absolute inset-0 m-auto opacity-50" src="/asset/images/logo.png" alt="logo vite et gourmand">
+      <img class="h-25 w-35 lg:h-30 lg:w-50 object-cover absolute inset-0 m-auto opacity-50" src="/asset/images/logo.png" alt="logo vite et gourmand">
     </div>
     <div>
-      <h1 class="relative z-10 font-h1 font-extrabold text-texte text-4xl"><?= strtoupper($titre) ?></h1>
+      <h1 class="relative z-10 font-h1 font-extrabold text-texte text-3xl lg:text-4xl"><?= strtoupper($titre) ?></h1>
     </div>
   </div>
 </div>
